ab913c8f70847a2f42eba2cfb89ee6834979974c
Roman De Rose <romandr37@gmail.com>
2022-04-06 17:53:33 -0300

    Favicon

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,18 @@ function App() {
 
   return (
     <div className="bg-gray-900 min-h-screen text-center text-purple-500 py-1">
-      <h1 className="text-5xl pt-3 mb-3 font-black">Todo List</h1>
+      <h1 className="text-5xl pt-4 mb-4 font-black">Todo List</h1>
       <form action="" className="w-4/5 mx-auto max-w-sm font-medium">
         <input
           value={newToDo}
           type="text"
-          className="w-full bg-transparent font-medium mt-1 mb-3 py-3 px-3 border-4 border-purple-500 rounded-2xl shadow-lg text-1xl placeholder-purple-500/60 transition-all focus:outline-none shadow-purple-500/30"
+          className="w-full bg-transparent font-medium mt-1 mb-4 py-3 px-3 border-4 border-purple-500 rounded-2xl shadow-lg text-1xl placeholder-purple-500/60 transition-all focus:outline-none shadow-purple-500/30"
           placeholder="Agenda una tarea..."
           onChange={handleToDoChange}
           required
         />
         <button
-          className="bg-purple-500 text-white shadow-purple-500/40 my-2 p-3 rounded-xl shadow-lg transition-all hover:shadow-purple-600/40 hover:bg-purple-600 active:scale-95"
+          className="bg-purple-500 text-white shadow-purple-500/40 mt-2 p-3 rounded-xl shadow-lg transition-all hover:shadow-purple-600/40 hover:bg-purple-600 active:scale-95"
           onClick={handleNewToDo}
         >
           Agregar tarea
